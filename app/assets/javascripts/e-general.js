9989ac24c8670d07088ad6011537fd5df6fe2ff3
johnlucia <johnmlucia@gmail.com>
2018-07-18 23:02:38 -0700

    coming along

---
+++ app/assets/javascripts/e-general.js
@@ -0,0 +1,6 @@
+$("#email-contact-button").click(function() {
+  $([document.documentElement, document.body]).animate({
+      scrollTop: $("#email-contact-form").offset().top
+  }, 1500);
+  return false;
+});
