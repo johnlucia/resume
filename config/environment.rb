168d10120549801a492f6145662626f6aa53a26f
johnlucia <johnmlucia@gmail.com>
2018-07-28 15:23:57 -0700

    mail

--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,13 @@ Rails.application.initialize!
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   html_tag.html_safe
 end
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'followrhythm.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,6 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -66,15 +65,14 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # ActionMailer::Base.smtp_settings = {
-  #   :user_name => 'your_sendgrid_username',
-  #   :password => 'your_sendgrid_password',
-  #   :domain => 'yourdomain.com',
-  #   :address => 'smtp.sendgrid.net',
-  #   :port => 587,
-  #   :authentication => :plain,
-  #   :enable_starttls_auto => true
-  # }
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'followrhythm.com',
+    :authentication => :plain,
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
