36382ef144f44f29e3f2d9cf22ebcb5796b21516
johnlucia <johnmlucia@gmail.com>
2018-07-28 14:23:29 -0700

    form submission

--- app/assets/javascripts/e-general.js
+++ app/assets/javascripts/e-general.js
@@ -4,3 +4,11 @@ $(".email-contact-button").click(function() {
   }, 1500);
   return false;
 });
+
+$( document ).ready(function() {
+  if ($('#error_explanation').length) {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#email-contact-form").offset().top
+    }, 1500);
+  }
+});
